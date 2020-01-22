fdf076abf33159244cb73f6ef5fda807cf62074a
saladuha <saladuha@nca.by>
2020-01-22 17:36:23 +0300

    config  Spring Security oAuth2 on front; add user session

---
+++ src/main/java/by/saladukha/sct2/controller/MainController.java
@@ -0,0 +1,41 @@
+package by.saladukha.sct2.controller;
+
+import by.saladukha.sct2.domain.User;
+import by.saladukha.sct2.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+/**
+ * Created by Zhenya Saladukha on 22.01.2020
+ * project: sct2
+ * mail: saladuha@nca.by
+ * skype: z.saladukha
+ */
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user) {
+        HashMap<Object, Object> data = new HashMap<>();
+
+        data.put("profile", user);
+        data.put("messages", messageRepo.findAll());
+
+        model.addAttribute("frontendData", data);
+        return "index";
+    }
+}
