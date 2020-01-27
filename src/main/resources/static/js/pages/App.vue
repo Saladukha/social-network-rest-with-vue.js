e1c4364799fb91de284b406bc35d22c1227e0d3c
saladuha <saladuha@nca.by>
2020-01-27 12:06:50 +0300

    connect WebSocket

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -5,12 +5,14 @@
         </div>
         <div v-else>
             <div>{{profile.name}}&nbsp;<a href="/logout">Logout</a></div>
-            <messages-list :messages="messages" />
+            <messages-list :messages="messages"/>
         </div>
     </div>
 </template>
 <script>
     import messagesList from 'components/messages/messageList.vue'
+    import {addHandler} from "util/ws";
+    import {getIndex} from "util/collections";
 
     export default {
         components: {
@@ -21,6 +23,16 @@
                 messages: frontendData.messages,
                 profile: frontendData.profile
             }
+        },
+        created() {
+            addHandler(data => {
+                let index = getIndex(this.messages, data.id)
+                if (index > -1) {
+                    this.messages.splice(index, 1, data)
+                } else {
+                    this.messages.push(data)
+                }
+            })
         }
     }
 </script>
