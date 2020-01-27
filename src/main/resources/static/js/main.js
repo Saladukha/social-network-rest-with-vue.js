1ece6d0841bd44bf4a9153f4291b79bd09cbe5f9
saladuha <saladuha@nca.by>
2020-01-27 17:23:11 +0300

    add style: Vuetify and Material Design

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
 import {connect} from "./util/ws";
+import vuetify from "./plagins/vuetify";
+import Vuetify from "vuetify";
 
 if (frontendData.profile) {
     connect()
 }
 
+Vue.use(Vuetify)
 Vue.use(VueResource);
 
 new Vue({
     el: '#app',
+    vuetify,
     render: a => a(App)
 });
